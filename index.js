7b13840ed24f7ee5a63d1754a7fc0f95dd35acca
ankitdas13 <ankit.das@razorpay.com>
2023-09-19 04:10:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+
+app.use(cors())
+
+app.get("/",(request, response)=>{
+    response.send('api test...')
+ })
+
+app.get("/posts", async (request, response)=>{
+   const data = await fetch("https://jsonplaceholder.typicode.com/posts")
+   const resp = await data.json();
+   response.json(resp)
+})
+
+app.listen(3000,()=>{
+    console.log(`server is running on port ${3000}`)
+})
