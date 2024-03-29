34e67788f15816b707b1cfe9447e3da50b2fea12
ankitdas13 <ankit.das@razorpay.com>
2024-03-30 03:02:35 +0530

    added dsl

---
+++ job dsl/nodedocker.groovy
@@ -0,0 +1,28 @@
+job("node js project ver 2"){
+    description('This project will clone node js and build and push it to docker hub')
+    scm {
+        git('https://github.com/ankitdas13/node-microservices.git') { node ->
+            node / gitConfigName('ankitdas13')
+            node / gitConfigEmail('ankit.das@razorpay.com')
+        }
+    }
+    wrappers { 
+        nodejs('node 18')
+    }
+    credentialsBinding {
+        usernamePassword('USERNAME_DOC', 'PASSWORD_DOC', 'dockerhubcred')
+    }
+    steps {
+        shell('npm install')
+        shell('docker login -u ${USERNAME_DOC} -p ${PASSWORD_DOC}')
+        dockerBuildAndPublish {
+            repositoryName('ankitrazorpay/node-jenkins-demo')
+            tag('${BUILD_NUMBER}')
+            registryCredentials('dockerhubcred')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+       }
+       shell('docker logout')
+    }
+}
